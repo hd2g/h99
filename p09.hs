8bc2e7b0ad52759354113ad2059afb130b4b9a92
hd2g <nagisa.159s.8673.trap@gmail.com>
2023-05-09 05:21:17 +0900

    solve p09 for Haskell

---
+++ p09.hs
@@ -0,0 +1,9 @@
+import Prelude hiding (pack)
+-- |
+-- >>> pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e']
+-- ["aaaa","b","cc","aa","d","eeee"]
+--
+pack :: [Char] -> [String]
+pack [] = []
+pack (x : xs) = (x : ys) : pack zs where
+  (ys, zs) = span (== x) xs
