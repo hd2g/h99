7183aaeaed9646308ea09110e06714f9405a21ae
hd2g <nagisa.159s.8673.trap@gmail.com>
2023-05-08 12:19:03 +0000

    solve p05 for Haskell

---
+++ p05.hs
@@ -0,0 +1,11 @@
+-- |
+-- >>> myReverse "A man, a plan, a canal, panama!"
+-- "!amanap ,lanac a ,nalp a ,nam A"
+--
+-- >>> myReverse [1,2,3,4]
+-- [4,3,2,1]
+--
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
